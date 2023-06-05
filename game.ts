faadf39ba6a104b0c4b31090290422267c88488c
Eylul Oktay <gulzadeoktay@gmail.com>
2023-06-05 10:47:24 -0400

    more functions added to game.ts

--- game.ts
+++ game.ts
@@ -3,32 +3,67 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-//this array will store each guessed shape sequence
-const guesses = [[]];
+    //this array will store each guessed shape sequence
+    let guesses = [[]];
+    let spacesOpen = 1;
+    let shapeSequence = "DFGHJ"
 
-//keys will correspond to certain shapes D-K 
-const keyPress = document.querySelectorAll(".keyboard-row button");
+    //keys will correspond to certain shapes D-K 
+    const keyPress = document.querySelectorAll(".keyboard-row button");
 
-for (let i = 0; i < keyPress.length; i++) {
-    keyPress[i].onClick = ({target}) => {
-        const shape = target.getAttribute("data-key");
-        getShapesGuessed(shape);
+    //returns the current sequence that user has entered
+    function getCurrentGuess() {
+        const numGuesses = guesses.length;
+        return guesses[numGuesses - 1];
     }
-};
 
-//returns the current sequence that user has entered
-function getCurrentGuess() {
-    const numGuesses = guesses.length;
-    return guesses[numGuesses - 1];
-}
+    //checks if the user has entered the required 5 letters into the sequence
+    function getShapesGuessed(shape){
+        const currentGuess = getCurrentGuess();
+        
+        if (currentGuess && guesses.length < 5){
+            guesses.push(shape);
 
-//checks if the user has entered the required 5 letters into the sequence
-function getShapesGuessed(shape){
-    const currentGuess = getCurrentGuess();
-    
-    if (currentGuess && guesses.length < 5){
-        guesses.push(shape);
+            const spaceAvailable = document.getElementById("String(1)");
+            spacesOpen += 1;
+
+            spacesOpen.textContent = shape;
+        }
     }
-}
 
+    function enteredSequence() {
+        const currentGuess = getCurrentGuess();
+        if (currentGuess.length != 5) {
+            window.alert("Please enter a sequence with 5 shapes!");
+        }
+
+        const currentShape = guesses.join(' ');
+        if (currentShape == shapeSequence) {
+            //link over to winninSCreen
+        }
+
+    }
+
+    function getShapeGrid() {
+        const gameGrid = document.getElementById("grid");
+
+        //loops through indexes in the grid
+        for (let i = 0; i < 25; i ++){
+            let block = document.createElement("div");
+            block.classList.add("block");
+            block.setAttribute("id", i + 1);
+            gameGrid.appendChild(block);
+        }
+    }
+
+    for (let i = 0; i < keyPress.length; i++) {
+        keyPress[i].onclick = ({target}) => {
+            const shape = target.getAttribute("data-key");
+            if (shape === 'enter') {
+                enteredSequence();
+                return;
+            }
+            getShapesGuessed(shape);
+        }
+    };
 });
